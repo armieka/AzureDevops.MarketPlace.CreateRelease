7014f11f0387be50c15cb8a72d61679d36495bde
Sietse Mooren <s.mooren@beslist.nl>
2019-06-25 21:09:41 +0200

    get definitionId working

--- index.ts
+++ index.ts
@@ -1,13 +1,39 @@
 import * as taskLib from 'azure-pipelines-task-lib/task';
+import "isomorphic-fetch";
+
+async function GetDefinitionIdAsync(headers: HeadersInit, projectName: string, releaseName: string) : Promise<number> {
+    return fetch('https://vsrm.dev.azure.com/beslistnl/' + projectName + '/_apis/release/definitions?api-version=5.0&searchText=' + releaseName + '&isExactNameMatch=true',
+        {
+            headers: headers,
+            method: 'GET'
+        })
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            throw Error(response.statusText);
+        })
+        .then(function (jsonResult) {
+            if (jsonResult.count != 1) {
+                throw Error('to many release definitions');
+            }
+            else {
+                return jsonResult.value[0].id;
+            }
+        });
+}
 
 async function run() {
     try {
-        const inputString: string = taskLib.getInput('samplestring', true);
-        if (inputString == 'bad') {
-            taskLib.setResult(taskLib.TaskResult.Failed, 'Bad input was given');
-            return;
-        }
-        console.log('Hello', inputString);
+        const projectName: string = taskLib.getInput('ProjectName', true);
+        const releaseName: string = taskLib.getInput('ReleaseName', true);
+        // const artifactEnvironment: string = taskLib.getInput('ArtifactEnvironment', true);
+        // const environment: string = taskLib.getInput('Environment', true);
+        // const attributes: string = taskLib.getInput('Attributes', true);
+        let token = Buffer.from('').toString('base64')
+        let headers: any = { 'Authorization': 'Basic ' + token };
+        let definitionId = await GetDefinitionIdAsync(headers, projectName, releaseName);
+        console.log(definitionId);
     }
     catch (err) {
         taskLib.setResult(taskLib.TaskResult.Failed, err.message);
